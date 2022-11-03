d83917478821088b0fff7c6ce8ee4497f69ac70e
Quentin Wittevrongel <quentin.witte@gmail.com>
2022-11-03 10:32:26 +0100

    Update adam rig node plugin version

--- hooks/tk-multi-launchapp/before_app_launch.py
+++ hooks/tk-multi-launchapp/before_app_launch.py
@@ -119,7 +119,7 @@ class BeforeAppLaunch(tank.Hook):
                     # Add Adam scripts.
                     self.addToEnvironmentEnd("PYTHONPATH", "P:\\shows\\ADAM\\td\\maya\\scripts")
                     # Add Adam rig nodes.
-                    self.addToEnvironmentEnd("MAYA_MODULE_PATH", "P:\\shows\\ADAM\\td\\productionPackages\\adamRigNodes-0.1.2\\module")
+                    self.addToEnvironmentEnd("MAYA_MODULE_PATH", "P:\\shows\\ADAM\\td\\productionPackages\\adamRigNodes-0.1.3\\module")
                     # Add Alembic 2.0.
                     self.addToEnvironmentEnd("MAYA_PLUG_IN_PATH", "P:\\shows\\ADAM\\td\\productionPackages\\alembic\\2.0\\maya2023\\plug-ins")
 
