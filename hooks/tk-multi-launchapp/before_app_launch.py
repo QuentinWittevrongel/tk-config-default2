a3baa3d6e1fe06adde7540f40d8382a0a6112061
Quentin <quentin.witte@gmail.com>
2023-03-28 09:18:22 +0200

    Switch to master modules

--- hooks/tk-multi-launchapp/before_app_launch.py
+++ hooks/tk-multi-launchapp/before_app_launch.py
@@ -140,7 +140,7 @@ class BeforeAppLaunch(tank.Hook):
         
             if(projectName == "ADAM"):
                 # Add Adam Houdini packages.
-                self.addToEnvironmentEnd('HOUDINI_PACKAGE_DIR', 'P:\\shows\\ADAM\\td\\houdini\\houdiniPackagesDev')
+                self.addToEnvironmentEnd('HOUDINI_PACKAGE_DIR', 'P:\\shows\\ADAM\\td\\houdini\\houdiniPackages')
 
         elif(software_entity["code"] == "Nuke"):
             self.addToEnvironmentBegin('NUKE_PATH', 'Z:\\P3DTools\\productionPackages\\nukeGizmos')
